8385b0ceb0ebcfae85d90d049f90feb72c9ff584
Dotan Nahum <jondotan@gmail.com>
2022-03-30 22:01:21 +0300

    .

--- src/lib.rs
+++ src/lib.rs
@@ -6,11 +6,16 @@ extern crate serde_yaml;
 pub mod data;
 pub mod runner;
 
+fn foo() -> i32 {
+    1
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
 
     #[test]
     fn test_foo() {
-        assert_eq!(0, 0);
+        assert_eq!(foo(), 1);
     }
 }
